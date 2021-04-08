161eafde37b08907ad5ab9e09bce7b4e4d49f63b
BianTanggui <mamincang@huawei.com>
2021-04-08 21:22:35 +0800

    Match-id-765ec7fd5a72548a588e986b794454e804519225

--- build/build.sh
+++ build/build.sh
@@ -61,13 +61,21 @@ function build_bin()
     echo "make hook"
     [ -d "${HOOKSRCDIR}/build" ] && rm -rf ${HOOKSRCDIR}/build
     mkdir ${HOOKSRCDIR}/build && cd ${HOOKSRCDIR}/build
-    go build -ldflags "-buildid=IdNetCheck" -trimpath ../${HOOKSRCNAME}
+    export CGO_ENABLED=1
+    export CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
+    export CGO_CPPFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
+    go build -buildmode=pie -ldflags "-buildid=IdNetCheck" -trimpath ../${HOOKSRCNAME}
+    strip main
     mv main ascend-docker-hook
 
     echo "make runtime"
     [ -d "${RUNTIMESRCDIR}/build" ] && rm -rf ${RUNTIMESRCDIR}/build
     mkdir ${RUNTIMESRCDIR}/build&&cd ${RUNTIMESRCDIR}/build
-    go build -ldflags "-buildid=IdNetCheck" -trimpath ../${RUNTIMESRCNAME}
+    export CGO_ENABLED=1
+    export CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
+    export CGO_CPPFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
+    go build -buildmode=pie  -ldflags "-buildid=IdNetCheck" -trimpath ../${RUNTIMESRCNAME}
+    strip main
     mv main ascend-docker-runtime
 }
 
