3051dc666d8f6918818ddeb09ef4e5b0f6ef6d3f
BianTanggui <sunchuhan@huawei.com>
2022-06-14 15:38:30 +0800

    Match-id-0564baf09bb178fd45f166def3a8d1aac178ff32

--- hook/main.go
+++ hook/main.go
@@ -374,6 +374,11 @@ func doPrestartHook() error {
 }
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 	log.SetPrefix(loggingPrefix)
 
 	if err := doPrestartHook(); err != nil {
