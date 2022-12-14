54849e7239f2559c178212dfd7b728b175293f40
BianTanggui <songxiangbing@huawei.com>
2022-12-14 17:24:46 +0800

    Match-id-860cd95ec8fb18ce00a4680ef0274f67cd2d5068

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-# Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
-# Description: ascend-docker-runtime构建脚本
+# Copyright (c) Huawei Technologies Co., Ltd. 2020-2022. All rights reserved.
+# Description: ascend-docker-runtime build script
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
 set -ex
 
 ROOT=$(cd $(dirname $0); pwd)/..
--- cli/src/u_mount.h
+++ cli/src/u_mount.h
@@ -1,7 +1,17 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
- * Description: ascend-docker-cli工具容器设备与驱动挂载模块头文件
-*/
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2022. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef _MOUNT_H
 #define _MOUNT_H
 
